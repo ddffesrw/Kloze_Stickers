4b93b02c4671dfea41d5d3de1101e9aa9aca99bb
John Axe <johnaxe.storage@gmail.com>
2026-01-24 22:56:08 +0300

    like butonu

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -14,7 +14,7 @@ export default function AuthPage() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider,
                 options: {
-                    redirectTo: `${window.location.origin}/`, // Redirect back to home
+                    redirectTo: window.location.origin, // Redirect back to home
                 },
             });
             if (error) throw error;
